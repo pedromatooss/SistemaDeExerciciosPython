78ceb291316d493c5e19e3cced90311357f0c35a
PEDRO LUCAS MORAIS MATOS <pedrolmatos.dev@gmail.com>
2026-03-31 11:08:49 -0300

    Atualização de Alguns Exercicios

--- ListaRepeticao.py
+++ ListaRepeticao.py
@@ -1,5 +1,28 @@
 def executar():
     print("ESTRUTURA DE REPETIÇÃO\n")
+    opcao = int(input("EXERCICIOS (1 ao 10) ou 0 para sair: "))
 
-    for i in range(1, 6):
-        print(f"Contagem: {i}")
+    match opcao:
+        case 1: 
+            print("Loop Simples------------")
+            loop = True
+            while loop: 
+                nota = int(input("digite uma nota de 0 a 10: "))
+                if (nota > 10 or nota < 0):
+                    print("nota invalida, tente novamente")
+                else: 
+                    print("Obrigado pela nota XD")
+                    loop = False
+        case 2: 
+            print("Usuario e senha----------")
+            loop = True
+            while loop: 
+                usuario = input("Digite seu Nome de usuario: ")   
+                senha = input("Digite a sua senha: ")           
+                if (usuario == senha):
+                    print("Senha igual ao Usuario, Digite uma nova senha: ")
+                else: 
+                    loop = False
+        case _:
+            print("opcao invalida!")
+            
--- ListaSequencial.py
+++ ListaSequencial.py
@@ -7,15 +7,18 @@ def executar():
             return
 
         case 1:
+            print("Enviar um numero-----------")
             x = int(input("Digite um numero: "))
             print(f"O numero é {x}")
 
         case 2:
+            print("Somar Números--------------")
             x = int(input("Digite um numero: "))
             y = int(input("Digite outro numero: "))
             print(f"Soma: {x + y}")
 
         case 3:
+            print("Calculo de nota-------------")
             n1 = int(input("Nota 1: "))
             n2 = int(input("Nota 2: "))
             n3 = int(input("Nota 3: "))
@@ -23,10 +26,12 @@ def executar():
             print(f"Média: {(n1+n2+n3+n4)/4}")
 
         case 4:
+            print("conversor de Metros----------")
             metros = int(input("Metros: "))
             print(f"Em cm: {metros * 100}")
 
         case 5:
+            print("calulo de Raio---------------")
             raio = int(input("Raio: "))
             print(f"Área: {3.14 * raio**2}")
 
